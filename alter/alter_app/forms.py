ad6f3a8c25e2847f2d29176cfbab48fa72dd23c8
aki <kieran.mortimer@gmail.com>
2021-06-19 12:52:19 +0200

    Styled django user reg form with bootstrap

--- alter/alter_app/forms.py
+++ alter/alter_app/forms.py
@@ -3,17 +3,58 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 # Create your forms here
-
-
-
-
 class RegistrationForm(UserCreationForm):
+    # Overwriting constructor for password fields to comply with bootstrap
+    password1 = forms.CharField(
+        label = 'Password',
+        widget = forms.PasswordInput(attrs={
+            'class': 'form-control'
+        })
+    )
+
+    password2 = forms.CharField(
+        label = 'Password',
+        widget = forms.PasswordInput(attrs={
+            'class': 'form-control'
+        })
+    )
+
+    # Custom User registration form 
     class Meta:
         model = User
+
         fields = [
             'username',
             'email',
             'password1',
             'password2'
         ]
+
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'type': 'text',
+                'id': 'RegisterUsername',
+                'aria-describedby': 'UsernameHelp',
+            }),
+
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'type': 'email',
+                'id': 'RegisterEmail',
+                'aria-describedby': 'RegisterEmailHelp'
+            }),
+
+            'password1': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'type': 'password',
+                'id': 'RegisterPassword',
+            }),
+
+            'password2': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'type': 'password',
+                'id': 'RegisterConfirmPassword'
+            }),
+        }
     

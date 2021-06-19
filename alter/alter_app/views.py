254b933e983b6eb24568e33eba30d284dee1d425
aki <kieran.mortimer@gmail.com>
2021-06-19 17:43:02 +0200

    placeholders for registation failures

--- alter/alter_app/views.py
+++ alter/alter_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .forms import RegistrationForm
 from .models import Species
 from psd_tools import PSDImage
@@ -10,6 +12,7 @@ import base64
 def index(request):
     return render(request, 'alter_app/index.html')
 
+
 # Upload Page
 def upload(request):
     # Querying DB for existing species
@@ -17,7 +20,9 @@ def upload(request):
 
     # Passing context to renderer
     context = {'species_list': species_list}
+
     return render(request, 'alter_app/upload.html', context)
+
 
 # Login Page
 def login(request):
@@ -31,14 +36,24 @@ def login(request):
         if user_creation_form.is_valid():
             user_creation_form.save()
 
+            # Creating a popup message when successful creation occurs
+            user = user_creation_form.cleaned_data.get('username')
+            messages.success(request, user + '\'s account successfully created')
+
+        else:
+            # TODO write error messages for invalid registrations
+            pass
+
     # Passing context to renderer
     context = {'user_creation_form': user_creation_form}
 
     return render(request, 'alter_app/login.html', context)
 
+
 # Gallery Page
 def gallery(request):
     return render(request, 'alter_app/gallery.html')
+
 
 # PSD Editor Page
 def editor(request):
